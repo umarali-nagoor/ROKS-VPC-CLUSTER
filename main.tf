6660c3f60998a76ed548f8e76494541fc6f3d452
Umar Ali <umarali@Umars-MacBook-Pro.local>
2025-06-26 16:08:11 +0530

    Added flavour

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ data "ibm_resource_group" "resource_group" {
 resource "ibm_container_vpc_cluster" "cluster" {
   name              = "roks-vpc-cluster-can-be-deleted"
   vpc_id            = ibm_is_vpc.vpc1.id
-  flavor            = "bx2.4x16"
   worker_count      = 3
   resource_group_id = data.ibm_resource_group.resource_group.id
-  kube_version      = "4.14.52_openshift" 
+  kube_version      = "4.3_openshift"
+  flavor            = "bx2.16x64"
   disable_public_service_endpoint = true
   zones {
     subnet_id = ibm_is_subnet.subnet1.id
